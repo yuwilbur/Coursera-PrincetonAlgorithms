e9518f19269721d7e3ffd3fbf7f87a1542ea5c58
Wilbur Yu <yuwilbur@gmail.com>
2016-02-22 16:02:10 -0800

    Added Line class to store number of existing lines to check for duplicates

--- 3J - Collinear Points/BruteCollinearPoints.java
+++ 3J - Collinear Points/BruteCollinearPoints.java
@@ -3,14 +3,46 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 
 public class BruteCollinearPoints {
+  private class Line {
+    public Line(Point point1, Point point2) {
+      this.slope = point1.slopeTo(point2);
+      if (this.slope != Double.POSITIVE_INFINITY) {
+        this.intercept = point1.x * slope - point1.y;
+      } else {
+        this.intercept = point1.x;
+      }
+    }
+    public boolean compareTo(Line line) {
+      return (slope == line.slope && intercept == line.intercept);
+    }
+    double slope = 0.0;
+    double intercept = 0.0;
+  }
+  private Line[] lines;
   private LineSegment[] segments;
   
   public BruteCollinearPoints(Point[] points) {
+    if (points.length == 0)
+      throw new java.lang.NullPointerException();
+    
+    lines = new Line[0];
     segments = new LineSegment[0];
     for(int i = 0; i < points.length; i++) {
       for(int j = i + 1; j < points.length; j++) {
+        if (points[i].compareTo(points[j]) == 0) // Equal points
+          throw new java.lang.IllegalArgumentException();
+        Line line = new Line(points[i], points[j]);
+        boolean duplicateFound = false;
+        for(int lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+          if (line.compareTo(lines[lineIndex])) {
+            duplicateFound = true;
+          }
+        }
+        if (duplicateFound)
+          continue;
+        
         Double slope = points[i].slopeTo(points[j]);
-        System.out.print(i + "." + j + "::" + slope + "::");
+        
         Point lowerPoint = points[i];
         Point upperPoint = points[i];
         if (upperPoint.compareTo(points[j]) < 0)
@@ -20,8 +52,6 @@ public class BruteCollinearPoints {
         int count = 2;
         for(int k = j + 1; k < points.length; k++) {
           if (points[j].slopeTo(points[k]) == slope) {
-            System.out.print(points[j].slopeTo(points[k]) + " ");
-            //addSegment(new LineSegment(points[j], points[k]));
             count++;
             if (upperPoint.compareTo(points[k]) < 0)
               upperPoint = points[k];
@@ -30,12 +60,8 @@ public class BruteCollinearPoints {
           }
         }        
         if (count >= 4) {
-          System.out.print("A:" + lowerPoint.slopeTo(upperPoint) + " ");
-          upperPoint.draw();
-          lowerPoint.draw();
-          addSegment(new LineSegment(upperPoint, lowerPoint));
+          addSegment(upperPoint, lowerPoint);
         }
-        System.out.println("");
       }
     }
   }
@@ -48,11 +74,17 @@ public class BruteCollinearPoints {
     return segments;
   }
   
-  private void addSegment(LineSegment segment) {
+  private void addSegment(Point upperPoint, Point lowerPoint) {
+    LineSegment segment = new LineSegment(upperPoint, lowerPoint);
     LineSegment[] newSegments = new LineSegment[segments.length + 1];
-    for(int i = 0; i < segments.length; ++i) {
+    Line line = new Line(upperPoint, lowerPoint);
+    Line[] newLines = new Line[lines.length + 1];
+    for(int i = 0; i < lines.length; ++i) {
+      newLines[i] = lines[i];
       newSegments[i] = segments[i];
     }
+    newLines[newLines.length - 1] = line;
+    lines = newLines;
     newSegments[newSegments.length - 1] = segment;
     segments = newSegments;
   }
@@ -72,14 +104,14 @@ public class BruteCollinearPoints {
     StdDraw.show(0);
     StdDraw.setXscale(0, 32768);
     StdDraw.setYscale(0, 32768);
-    StdDraw.setPenRadius(0.02);
+    StdDraw.setPenRadius(0.01);
     for (Point p : points) {
       p.draw();
     }
     StdDraw.show();
     
     // print and draw the line segments
-    StdDraw.setPenRadius(0.01);
+    StdDraw.setPenRadius(0.005);
     BruteCollinearPoints collinear = new BruteCollinearPoints(points);
     for (LineSegment segment : collinear.segments()) {
       StdOut.println(segment);
